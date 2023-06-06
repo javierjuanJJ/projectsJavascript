fd75a4b0278c4ee2194bb52ce6742ff4dcb68493
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-06-06 11:46:10 +0200

    Commit 1

---
+++ project1/script.js
@@ -0,0 +1,27 @@
+const newVariable = 'Hellow World';
+console.log(newVariable);
+
+const anotherVariable = 20;
+console.log(anotherVariable);
+
+var variable1 = 20;
+console.log(variable1);
+
+var variable2 = 20;
+console.log(variable2);
+
+var variable3 = 20;
+console.log(variable3);
+
+let counter = 0;
+console.log(counter);
+
+counter = counter + 1;
+console.log(counter);
+
+counter++;
+console.log(counter);
+
+console.log(typeof(counter));
+console.log(typeof(newVariable));
+
